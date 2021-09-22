ff9dbc4b0bf15458d160e09117c2d65625e78c44
Jorge Barreto <jsambarreto@gmail.com>
2021-09-22 10:28:32 -0300

    Alt parameters

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,8 @@ import 'package:flutter/material.dart';
 import 'pages/home_page.dart';
 
 void main() {
-  String? id = '1000000006'; // Uri.base.queryParameters["id"];
-  String? dsSecretaria =
-      'escolas'; //  Uri.base.queryParameters["dsSecretaria"];
+  String? id = Uri.base.queryParameters["id"];
+  String? dsSecretaria = Uri.base.queryParameters["dsSecretaria"];
 
   runApp(HomePage(
     dsSecretaria: dsSecretaria,
